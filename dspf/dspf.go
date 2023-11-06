fddfce90539e9c90ba3c792da9345b8adf637751
Leandro Rometsch <leandro@perun.network>
2023-11-06 18:21:50 +0100

    fix dspf eval logic

--- dspf/dspf.go
+++ dspf/dspf.go
@@ -12,6 +12,12 @@ type DSPF struct {
 	baseDPF dpf.DPF // The base DPF used to construct the DSPF
 }
 
+func NewDSPFFactory(baseDPF dpf.DPF) *DSPF {
+	return &DSPF{
+		baseDPF: baseDPF,
+	}
+}
+
 // Gen generates keys for a DSPFt given t special points and non-zero elements.
 func (d *DSPF) Gen(specialPoints []*big.Int, nonZeroElements []*big.Int) (Key, Key, error) {
 	// Check if the inputs are valid: same length and non-nil
@@ -49,33 +55,36 @@ func (d *DSPF) Gen(specialPoints []*big.Int, nonZeroElements []*big.Int) (Key, K
 }
 
 // Eval evaluates the DSPFt on a given point x.
-func (d *DSPF) Eval(dspfKey DSPFKey, x *big.Int) (*big.Int, error) {
-	// Deserialize the dspfKey to get the individual DPF keys
-	dpfKeys, err := deserializeDSPFKey(dspfKey)
-	if err != nil {
-		return nil, err
-	}
-
-	// Evaluate each DPF key and sum the results
-	sum := big.NewInt(0)
-	for _, key := range dpfKeys {
-		y, err := d.dpf.Eval(key, x)
+func (d *DSPF) Eval(dspfKey Key, x *big.Int) ([]*big.Int, error) {
+	ys := make([]*big.Int, len(dspfKey.DPFKeys))
+	for i, key := range dspfKey.DPFKeys {
+		y, err := d.baseDPF.Eval(key, x)
 		if err != nil {
 			return nil, err
 		}
-		sum.Add(sum, y)
+		ys[i] = y
 	}
-	return sum, nil
+	return ys, nil
 }
 
-// CombineResults combines the results from the evaluations of DSPF keys.
-func (d *dspfImpl) CombineResults(results ...*big.Int) *big.Int {
+// CombineResults combines the results from the evaluations of DSPF key.
+func (d *DSPF) CombineResults(y1 []*big.Int, y2 []*big.Int) (*big.Int, error) {
+	if len(y1) != len(y2) {
+		return nil, errors.New("length of y1 and y2 must match")
+	}
+
+	nonZeroPointFound := false
 	combined := big.NewInt(0)
-	for _, r := range results {
-		combined.Add(combined, r)
-	}
-	return combined
-}
+	zero := big.NewInt(0)
+	for i, y := range y1 {
+		res := d.baseDPF.CombineResults(y, y2[i])
 
-// We would also need to define the DSPFKey structure and methods for serialization/deserialization.
-// ...
+		if res.Cmp(zero) != 0 && !nonZeroPointFound {
+			nonZeroPointFound = true
+			combined.Add(combined, res)
+		} else if res.Cmp(zero) != 0 && nonZeroPointFound {
+			return nil, errors.New("multiple non-zero elements found for this x")
+		}
+	}
+	return combined, nil
+}
