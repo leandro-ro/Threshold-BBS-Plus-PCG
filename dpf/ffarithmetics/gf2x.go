907d1907d0633b697593d9533a27b601e492c3a6
Leandro Rometsch <leandro@perun.network>
2023-11-06 20:30:30 +0100

    fixed add and mult

--- dpf/ffarithmetics/gf2x.go
+++ dpf/ffarithmetics/gf2x.go
@@ -5,24 +5,17 @@
 package ffarithmetics
 
 import (
-	"bytes"
-	"encoding/csv"
+	"errors"
 	"fmt"
-	"io"
 	"math/big"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
-// Define the directory and filename for caching the polynomials
-const dirPath = ""
-const fileName = "cached_polynomials.csv"
+// Irreducible Polynomials for GF(2^m) taken from "Table of Low-Weight Binary Irreducible Polynomials"
+// by Gadiel Seroussi, https://www.hpl.hp.com/techreports/98/HPL-98-135.pdf
 
-var fullPathToPolys = filepath.Join(dirPath, fileName)
+const IrreduciblePoly128 = "340282366920938463463374607431768211590"                                        // "x^128 + x^7 + x^2 + x + 1"
+const IrreduciblePoly192 = "6277101735386680763835789423207666416102355444464034513030"                     // "x^192 + x^7 + x^2 + x + 1"
+const IrreduciblePoly256 = "115792089237316195423570985008687907853269984665640564039457584007913129640998" // "x^256 + x^10 + x^5 + x^2 + 1"
 
 // GF2M represents a field GF(2^m).
 type GF2M struct {
@@ -30,150 +23,167 @@ type GF2M struct {
 }
 
 // NewGF2M constructs a new GF2M struct which can then be used for calculations inside GF(2^m).
-// It uses a given m to find or generate an irreducible polynomial for GF(2^m).
-// The polynomial is then stored as a big.Int in the Modulus field.
-// If NewGF2M is called for an unseen m, the generation of an irreducible polynomial might take a moment.
-// For m's that NewGF2M has already seen, their respective irreducible polynomials are cached inside polynomials.csv.
-// You can also use polynomials.csv to pre-define polynomials.
 func NewGF2M(m int) (*GF2M, error) {
-	// Check that m is an odd prime number
-	if m < 2 || !big.NewInt(int64(m)).ProbablyPrime(0) || m%2 == 0 {
-		return nil, fmt.Errorf("m must be an odd prime number")
+	mod := big.NewInt(0)
+	switch m {
+	case 128:
+		mod.SetString(IrreduciblePoly128, 10)
+	case 192:
+		mod.SetString(IrreduciblePoly192, 10)
+	case 256:
+		mod.SetString(IrreduciblePoly256, 10)
+	default:
+		return nil, errors.New("m only supported for 128, 192, or 256")
 	}
-
-	// Check if polynomial for m exists in CSV
-	poly, err := readPolyFromCSV(m)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read polynomials from file: %v", err)
-	}
-	if poly == "" {
-		// Call SageMath to get polynomial
-		poly, err = GetIrreduciblePolynomial(m)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get irreducible polynomial: %v", err)
-		}
-		writePolyToCSV(m, poly)
-	}
-
-	// Convert polynomial to big.Int
-	modulus := polyStringToBigInt(poly)
 
 	return &GF2M{
-		Modulus: modulus,
+		Modulus: mod,
 	}, nil
 }
 
-// Add performs addition in GF(2^m).
-// It takes two big.Int values a and b, and returns their sum modulo g.Modulus.
+// Add performs bitwise XOR which is addition in GF(2^m).
 func (g *GF2M) Add(a, b *big.Int) *big.Int {
-	result := new(big.Int).Add(a, b)
-	result.Mod(result, g.Modulus)
+	result := new(big.Int).Xor(a, b)
 	return result
 }
 
 // Mul performs multiplication in GF(2^m).
 // It takes two big.Int values a and b, and returns their product modulo g.Modulus.
 func (g *GF2M) Mul(a, b *big.Int) *big.Int {
-	result := new(big.Int).Mul(a, b)
-	result.Mod(result, g.Modulus)
-	return result
+	result := g.karatsuba(a, b)
+	return g.reduce(result)
 }
 
-// Inv finds the multiplicative inverse of a in GF(2^m).
-// It returns nil if a is not coprime to g.Modulus.
-func (g *GF2M) Inv(a *big.Int) *big.Int {
-	result := new(big.Int).ModInverse(a, g.Modulus)
-	return result
-}
-
-// GetIrreduciblePolynomial fetches the irreducible polynomial for a given m using SageMath.
-// Returns an error if the polynomial could not be generated.
-func GetIrreduciblePolynomial(m int) (string, error) {
-	cmd := exec.Command("sage", "-c", fmt.Sprintf("print(GF(2^%d, 'x').modulus())", m))
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
+// ExpBySquaringFast computes a^exp in GF(2^m) using the square-and-multiply algorithm.
+func (g *GF2M) ExpBySquaringFast(a *big.Int, exp *big.Int) (*big.Int, error) {
+	if a.Sign() == 0 {
+		return nil, errors.New("cannot raise zero to a power")
 	}
-	result := strings.TrimSpace(out.String())
+	// Start with the multiplicative identity in GF(2^m).
+	result := big.NewInt(1)
+	base := new(big.Int).Set(a)
+
+	// Convert exp to binary representation for the square-and-multiply algorithm.
+	for exp.BitLen() > 0 {
+		if exp.Bit(0) == 1 { // If the least significant bit of exp is 1, multiply by the current base.
+			result = g.Mul(result, base)
+		}
+		fmt.Println("alive")
+
+		base = g.Square(base) // Square the base at each step.
+		exp.Rsh(exp, 1)       // Right shift exp by 1 (divide by 2).
+	}
+	fmt.Println("returned")
+
 	return result, nil
 }
 
-// readPolyFromCSV reads the irreducible polynomial for a given m from a CSV file.
-// If the file or the polynomial for the given m doesn't exist, returns an error.
-func readPolyFromCSV(m int) (string, error) {
-	file, err := os.Open(fullPathToPolys)
-	if err != nil {
-		if os.IsNotExist(err) {
-			// Create an empty file if it does not exist
-			file, err = os.Create(fullPathToPolys)
-			if err != nil {
-				return "", err
-			}
-		} else {
-			return "", err
-		}
+// Inv computes the multiplicative inverse of a in GF(2^m) using Fermat's Little Theorem.
+func (g *GF2M) Inv(a *big.Int) (*big.Int, error) {
+	if a.Sign() == 0 {
+		return nil, errors.New("zero does not have a multiplicative inverse in GF(2^m)")
 	}
-	defer file.Close()
 
-	reader := csv.NewReader(file)
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return "", err
-		}
-		if record[0] == fmt.Sprintf("%d", m) {
-			return record[1], nil
-		}
-	}
-	return "", nil
+	// We want to compute a^(2^m - 2), which is the inverse of a in GF(2^m).
+	m := g.Modulus.BitLen() - 1                                                      // Since the modulus represents x^m, its bit length is m+1.
+	exp := new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), uint(m)), big.NewInt(2)) // Compute 2^m - 2.
+
+	return g.ExpBySquaringFast(a, exp)
 }
 
-// writePolyToCSV writes the irreducible polynomial for a given m to a CSV file.
-func writePolyToCSV(m int, polyStr string) {
-	file, err := os.OpenFile(fullPathToPolys, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return
-	}
-	defer file.Close()
+// Square squares an element in GF(2^m).
+func (g *GF2M) Square(a *big.Int) *big.Int {
+	// The result starts at 0.
+	result := new(big.Int)
 
-	writer := csv.NewWriter(file)
-	writer.Write([]string{fmt.Sprintf("%d", m), polyStr})
-	writer.Flush()
+	// Iterate over each bit of 'a'.
+	for i := 0; i < a.BitLen(); i++ {
+		if a.Bit(i) == 1 {
+			// For each bit set in 'a', set the bit at position 2*i in the result.
+			result = result.SetBit(result, 2*i, 1)
+		}
+	}
+
+	// Reduce the result to ensure it stays within the field.
+	return g.reduce(result)
 }
 
-// polyStringToBigInt converts a polynomial string representation to its corresponding big.Int value.
-// Assumes the polynomial string is in the form e.g. "x^129 + x^5 + 1".
-func polyStringToBigInt(polyStr string) *big.Int {
-	// Regular expression to match terms like x^129
-	re := regexp.MustCompile(`x\^(\d+)`)
-	matches := re.FindAllStringSubmatch(polyStr, -1)
+// karatsuba is an internal helper function that performs the Karatsuba algorithm.
+func (g *GF2M) karatsuba(a, b *big.Int) *big.Int {
+	// Base case for the recursion
+	if len(a.Bits()) < 2 || len(b.Bits()) < 2 {
+		return g.simpleMultiply(a, b)
+	}
 
-	result := big.NewInt(0)
-	temp := big.NewInt(0)
+	// Calculate the size of the numbers
+	m := min(a.BitLen(), b.BitLen())
+	m2 := m / 2
 
-	for _, match := range matches {
-		// Parse the exponent part
-		exponent, err := strconv.Atoi(match[1])
-		if err != nil {
-			continue // skip malformed terms
+	// Split the digit sequences in the middle
+	low1 := new(big.Int).Mod(a, new(big.Int).Lsh(big.NewInt(1), uint(m2)))
+	high1 := new(big.Int).Rsh(a, uint(m2))
+	low2 := new(big.Int).Mod(b, new(big.Int).Lsh(big.NewInt(1), uint(m2)))
+	high2 := new(big.Int).Rsh(b, uint(m2))
+
+	// 3 calls made to numbers approximately half the size
+	z0 := g.karatsuba(low1, low2)
+	z1 := g.karatsuba(new(big.Int).Xor(low1, high1), new(big.Int).Xor(low2, high2))
+	z2 := g.karatsuba(high1, high2)
+
+	// Computing the result
+	temp := new(big.Int).Xor(z1, z2)
+	temp.Xor(temp, z0)
+	middle := new(big.Int).Lsh(temp, uint(m2))
+	high := new(big.Int).Lsh(z2, uint(2*m2))
+	result := new(big.Int).Xor(new(big.Int).Xor(high, middle), z0)
+
+	return g.reduce(result)
+}
+
+// reduce reduces a *big.Int modulo the irreducible polynomial of GF2M.
+func (g *GF2M) reduce(p *big.Int) *big.Int {
+	// Confirm the highest degree term of the modulus is set.
+	if g.Modulus.Bit(g.Modulus.BitLen()-1) != 1 {
+		panic("modulus is not a monic polynomial")
+	}
+
+	mod := new(big.Int).Set(g.Modulus)
+	modDegree := mod.BitLen() - 1
+
+	// Create a copy of p to avoid modifying the original value
+	pReduced := new(big.Int).Set(p)
+
+	// Perform polynomial long division (simulated with bitwise operations)
+	for pReduced.BitLen() >= modDegree {
+		// Calculate the shift amount based on the current degree of pReduced.
+		shift := uint(pReduced.BitLen() - modDegree)
+
+		// Create a shifted version of the modulus to XOR with pReduced.
+		t := new(big.Int).Lsh(mod, shift)
+
+		// XOR pReduced with the shifted modulus t to perform reduction.
+		pReduced.Xor(pReduced, t)
+
+		// Ensure that the degree is reduced.
+		if pReduced.BitLen() > p.BitLen() {
+			panic("degree of polynomial was not reduced - algorithm incorrect or inputs invalid")
 		}
-
-		// Calculate 2^exponent
-		temp.Exp(big.NewInt(2), big.NewInt(int64(exponent)), nil)
-
-		// Add it to the result
-		result.Or(result, temp)
 	}
 
-	// Check for the constant term "+ 1" and add it to the result if it exists
-	if regexp.MustCompile(`\+ 1`).MatchString(polyStr) {
-		result.Or(result, big.NewInt(1))
-	}
+	return pReduced
+}
 
+// simpleMultiply performs polynomial multiplication without carry,
+// which is essentially bit-wise AND operation followed by XOR for addition
+func (g *GF2M) simpleMultiply(a, b *big.Int) *big.Int {
+	result := new(big.Int)
+	for i := 0; i < b.BitLen(); i++ {
+		if b.Bit(i) == 1 {
+			// Shift 'a' to the left 'i' places, which is equivalent to multiplying by x^i
+			temp := new(big.Int).Lsh(a, uint(i))
+			// XOR the result with 'temp', simulating polynomial addition
+			result.Xor(result, temp)
+		}
+	}
 	return result
 }
--- dpf/ffarithmetics/gf2x_test.go
+++ dpf/ffarithmetics/gf2x_test.go
@@ -2,6 +2,8 @@ package ffarithmetics
 
 // Importing necessary packages
 import (
+	"crypto/rand"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"math/big"
 	"pcg-master-thesis/dpf"
@@ -10,72 +12,101 @@ import (
 
 func TestNewGF2M(t *testing.T) {
 	// Test for valid m
-	g, err := NewGF2M(7)
+	g, err := NewGF2M(128)
 	if err != nil {
-		t.Errorf("NewGF2M(7) returned an error: %v", err)
+		t.Errorf("NewGF2M(128) returned an error: %v", err)
 	}
 	if g.Modulus == nil {
-		t.Errorf("NewGF2M(7) returned nil Modulus")
+		t.Errorf("NewGF2M(128) returned nil Modulus")
 	}
 }
 
-func TestGF2MAdd(t *testing.T) {
-	g, err := NewGF2M(131)
+func TestGF2MSubEqAdd(t *testing.T) {
+	g, err := NewGF2M(128)
 	if err != nil {
-		t.Errorf("NewGF2M(129) returned an error: %v", err)
+		t.Errorf("NewGF2M(128) returned an error: %v", err)
 	}
-	a := big.NewInt(10)
-	b := big.NewInt(20)
+	a := big.NewInt(1261)
+	b := big.NewInt(1261)
 
 	result := g.Add(a, b)
-	expectedResult := big.NewInt(30)
+	expectedResult := big.NewInt(0)
 	if result.Cmp(expectedResult) != 0 {
-		t.Errorf("Add(10, 20) = %v; want %v", result, expectedResult)
+		t.Errorf("Add(1261, 1261) = %v; want %v", result, expectedResult)
 	}
 }
 
 func TestGF2MMul(t *testing.T) {
-	g, err := NewGF2M(131)
+	gf2m, err := NewGF2M(128)
 	if err != nil {
-		t.Errorf("NewGF2M(129) returned an error: %v", err)
+		t.Fatalf("Failed to create GF2M: %v", err)
 	}
-	a := big.NewInt(10)
-	b := big.NewInt(20)
 
-	result := g.Mul(a, b)
-	expectedResult := big.NewInt(200) // Assuming modulus is large enough
-	if result.Cmp(expectedResult) != 0 {
-		t.Errorf("Mul(10, 20) = %v; want %v", result, expectedResult)
+	// Test the associative property a*(b*c) == (a*b)*c
+	a := big.NewInt(123456789)
+	b := big.NewInt(987654321)
+	c := big.NewInt(192837465)
+
+	ab := gf2m.Mul(a, b)
+	abc := gf2m.Mul(ab, c)
+
+	bc := gf2m.Mul(b, c)
+	abc2 := gf2m.Mul(a, bc)
+
+	if abc.Cmp(abc2) != 0 {
+		t.Errorf("Associative property of multiplication failed: (a*b)*c != a*(b*c)")
+	}
+
+	// Test the distributive property a*(b+c) == (a*b) + (a*c)
+	bPlusC := gf2m.Add(b, c)
+	aTimesBPlusC := gf2m.Mul(a, bPlusC)
+
+	aTimesB := gf2m.Mul(a, b)
+	aTimesC := gf2m.Mul(a, c)
+	aTimesBPlusATimesC := gf2m.Add(aTimesB, aTimesC)
+
+	if aTimesBPlusC.Cmp(aTimesBPlusATimesC) != 0 {
+		t.Errorf("Distributive property of multiplication failed: a*(b+c) != (a*b) + (a*c)")
+	}
+
+	// Test multiplication by 1 (the multiplicative identity)
+	one := big.NewInt(1)
+	aTimesOne := gf2m.Mul(a, one)
+
+	if a.Cmp(aTimesOne) != 0 {
+		t.Errorf("Multiplication by one failed: a * 1 != a")
+	}
+
+	// Test multiplication by 0
+	zero := big.NewInt(0)
+	aTimesZero := gf2m.Mul(a, zero)
+
+	if zero.Cmp(aTimesZero) != 0 {
+		t.Errorf("Multiplication by zero failed: a * 0 != 0")
 	}
 }
 
 func TestGF2MInv(t *testing.T) {
-	g, err := NewGF2M(131)
+	gf2m, err := NewGF2M(128)
 	if err != nil {
-		t.Errorf("NewGF2M(129) returned an error: %v", err)
-	}
-	a := big.NewInt(4)
-	result := g.Inv(a)
-	if result == nil {
-		t.Errorf("Inv(4) returned nil")
+		t.Fatalf("Failed to create GF2M: %v", err)
 	}
 
-	// Validate the inverse by multiplying it with the original number and reducing modulo the field
-	check := g.Mul(a, result)
-	if check.Cmp(big.NewInt(1)) != 0 {
-		t.Errorf("Inv(3) did not return a valid multiplicative inverse. Got %v", result)
-	}
-}
+	// Calculate the inverse of 'a'
+	a := big.NewInt(123456789)
 
-func TestGF2MInvNonCoprime(t *testing.T) {
-	g, err := NewGF2M(131)
+	aInv, err := gf2m.Inv(a)
 	if err != nil {
-		t.Errorf("NewGF2M(129) returned an error: %v", err)
+		t.Errorf("Failed to find the inverse of a: %v", err)
 	}
-	a := big.NewInt(3)
-	result := g.Inv(a)
-	if result != nil {
-		t.Errorf("A is not coprime to modulus")
+
+	// Multiply 'a' by its inverse and reduce it
+	result := gf2m.Mul(a, aInv)
+
+	// The result should be '1'
+	if result.Cmp(big.NewInt(1)) != 0 {
+		t.Errorf("Multiplicative inverse property failed for a: %s, a^-1: %s, a * a^-1: %s",
+			a.Text(16), aInv.Text(16), result.Text(16))
 	}
 }
 
@@ -97,7 +128,7 @@ func TestArithmetics(t *testing.T) {
 	}
 
 	sum := gf2m.Add(alice, bob)
-	sumInv := gf2m.Inv(sum)
+	sumInv, _ := gf2m.Inv(sum)
 	w := gf2m.Mul(sumInv, b)
 
 	recAlice := gf2m.Mul(w, alice)
@@ -106,4 +137,21 @@ func TestArithmetics(t *testing.T) {
 	rec := gf2m.Add(recAlice, recBob)
 
 	assert.Equal(t, b, rec)
+}
+
+// randNonZeroBigInt generates a random non-zero big.Int less than a given bit length.
+func randNonZeroBigInt(bitLen int) (*big.Int, error) {
+	if bitLen <= 0 {
+		return nil, errors.New("bit length must be positive")
+	}
+
+	randBigInt := new(big.Int)
+	var err error
+	for randBigInt.Sign() == 0 { // Ensure it's non-zero
+		randBigInt, err = rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), uint(bitLen)))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return randBigInt, nil
 }
