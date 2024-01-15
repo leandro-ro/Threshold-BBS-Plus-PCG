a88e1ee7f8d89035dcce477a1ad6c582a052f268
Leandro Rometsch <leandro@perun.network>
2024-01-15 16:08:51 +0100

    refactoring

--- pcg/utils.go
+++ pcg/utils.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"pcg-master-thesis/pcg/poly"
 	"runtime"
+	"sort"
 	"sync"
 )
 
@@ -340,4 +341,331 @@ type polyResult struct {
 	aIndex  int
 	bIndex  int
 	product *poly.Polynomial
+}
+
+// evalFinalShare evaluates the final share of the PCG for the given polynomial.
+// This function effectively calculates the inner product between the given polynomial and the random polynomials in div.
+func (p *PCG) evalFinalShare(u, rand []*poly.Polynomial, div *poly.Polynomial) (*poly.Polynomial, error) {
+	numCores := runtime.NumCPU()
+	tasks := make(chan evalFinalShareTask, numCores)
+	results := make(chan evalFinalShareResult, p.c)
+
+	var wg sync.WaitGroup
+	worker := func() {
+		defer wg.Done()
+		for task := range tasks {
+			prod, err := poly.Mul(task.oprand, task.wPoly) // rand[r] and u[r] are swapped in this case
+			if err != nil {
+				results <- evalFinalShareResult{nil, err}
+				return
+			}
+
+			remainder, err := prod.Mod(div)
+			results <- evalFinalShareResult{remainder, err}
+		}
+	}
+
+	for i := 0; i < numCores; i++ {
+		wg.Add(1)
+		go worker()
+	}
+
+	go func() {
+		for r := 0; r < p.c; r++ {
+			tasks <- evalFinalShareTask{0, 0, rand[r], u[r], div, false} // Indices and isLastIndex are not used here
+		}
+		close(tasks)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	ai := poly.NewEmpty()
+	for i := 0; i < p.c; i++ {
+		result := <-results
+		if result.err != nil {
+			return nil, result.err
+		}
+		ai.Add(result.poly)
+	}
+
+	return ai, nil
+}
+
+// evalFinalShare2D evaluates the final share of the PCG for the given polynomial.
+// This function effectively calculates the inner product between the given polynomial and the random polynomials in div.
+func (p *PCG) evalFinalShare2D(w [][]*poly.Polynomial, oprand []*poly.Polynomial, div *poly.Polynomial) (*poly.Polynomial, error) {
+	numCores := runtime.NumCPU()
+	tasks := make(chan evalFinalShareTask, numCores)
+	results := make(chan evalFinalShareResult, numCores)
+
+	var wg sync.WaitGroup
+	worker := func() {
+		defer wg.Done()
+		for task := range tasks {
+			var result evalFinalShareResult
+			if task.isLastIndex {
+				remainder, err := task.wPoly.Mod(task.div)
+				result = evalFinalShareResult{remainder, err}
+			} else {
+				oprandMod, err := task.oprand.Mod(task.div)
+				if err != nil {
+					results <- evalFinalShareResult{nil, err}
+					return
+				}
+
+				prod, err := poly.Mul(oprandMod, task.wPoly)
+				if err != nil {
+					results <- evalFinalShareResult{nil, err}
+					return
+				}
+
+				remainder, err := prod.Mod(task.div)
+				result = evalFinalShareResult{remainder, err}
+			}
+			results <- result
+		}
+	}
+
+	for i := 0; i < numCores; i++ {
+		wg.Add(1)
+		go worker()
+	}
+
+	go func() {
+		for j := 0; j < p.c; j++ {
+			for k := 0; k < p.c; k++ {
+				currentIndex := j*p.c + k
+				isLastIndex := currentIndex == p.c*p.c-1
+				task := evalFinalShareTask{j, k, oprand[currentIndex], w[j][k], div, isLastIndex}
+				tasks <- task
+			}
+		}
+		close(tasks)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	alphai := poly.NewEmpty()
+	for range w {
+		for range w[0] {
+			result := <-results
+			if result.err != nil {
+				return nil, result.err
+			}
+			alphai.Add(result.poly)
+		}
+	}
+
+	return alphai, nil
+}
+
+// evalVOLEwithSeed evaluates the VOLE correlation with the given seed.
+func (p *PCG) evalVOLEwithSeed(u []*poly.Polynomial, seedSk *bls12381.Fr, seedDSPFKeys [][][]*DSPFKeyPair, seedIndex int, div *poly.Polynomial) ([]*poly.Polynomial, error) {
+	utilde := make([]*poly.Polynomial, p.c)
+	for r := 0; r < p.c; r++ {
+		ur := u[r].DeepCopy()    // We need unmodified u[r] later on, so we copy it
+		ur.MulByConstant(seedSk) // u[r] * sk[i]
+
+		for i := 0; i < p.n; i++ {
+			if i == seedIndex {
+				for j := 0; j < p.n; j++ {
+					if i != j {
+						eval0, err := p.dspfN.FullEvalFast(seedDSPFKeys[i][j][r].Key0)
+						if err != nil {
+							return nil, err
+						}
+						eval0Aggregated := aggregateDSPFoutput(eval0) // TODO: Workaround... make this more elegant
+						eval0Poly := poly.NewFromFr(eval0Aggregated)
+						ur.Add(eval0Poly)
+
+						eval1, err := p.dspfN.FullEvalFast(seedDSPFKeys[j][i][r].Key1)
+						if err != nil {
+							return nil, err
+						}
+						eval1Aggregated := aggregateDSPFoutput(eval1) // TODO: Workaround... make this more elegant
+						eval1Poly := poly.NewFromFr(eval1Aggregated)
+						ur.Add(eval1Poly)
+					}
+				}
+			}
+		}
+		utilde[r] = ur
+	}
+	return utilde, nil
+}
+
+// evalOLEwithSeed evaluates the OLE correlation with the given seed.
+func (p *PCG) evalOLEwithSeed(u, v []*poly.Polynomial, seedDSPFKeys [][][][]*DSPFKeyPair, seedIndex int, div *poly.Polynomial) ([][]*poly.Polynomial, error) {
+	w := make([][]*poly.Polynomial, p.c)
+	for r := 0; r < p.c; r++ {
+		w[r] = make([]*poly.Polynomial, p.c)
+		for s := 0; s < p.c; s++ {
+			wrs, err := poly.Mul(u[r], v[s]) // u an r are t-sparse -> t*t complexity
+			if err != nil {
+				return nil, err
+			}
+			for i := 0; i < p.n; i++ {
+				if i == seedIndex {
+					for j := 0; j < p.n; j++ {
+						if i != j { // Ony cross terms
+							eval0, err := p.dspf2N.FullEvalFast(seedDSPFKeys[i][j][r][s].Key0)
+							if err != nil {
+								return nil, err
+							}
+							eval0Aggregated := aggregateDSPFoutput(eval0) // TODO: Workaround... make this more elegant
+							eval0Poly := poly.NewFromFr(eval0Aggregated)
+							wrs.Add(eval0Poly) // N
+
+							eval1, err := p.dspf2N.FullEvalFast(seedDSPFKeys[j][i][r][s].Key1)
+							if err != nil {
+								return nil, err
+							}
+							eval1Aggregated := aggregateDSPFoutput(eval1) // TODO: Workaround... make this more elegant
+							eval1Poly := poly.NewFromFr(eval1Aggregated)
+							wrs.Add(eval1Poly) // N
+						}
+					}
+				}
+			}
+			w[r][s] = wrs
+		}
+	}
+	return w, nil
+}
+
+// embedVOLECorrelations embeds VOLE correlations into DSPF keys.
+func (p *PCG) embedVOLECorrelations(omega [][][]*big.Int, beta [][][]*bls12381.Fr, skShares []*bls12381.Fr) ([][][]*DSPFKeyPair, error) {
+	U := init3DSliceDspfKey(p.n, p.n, p.c)
+	for i := 0; i < p.n; i++ {
+		for j := 0; j < p.n; j++ {
+			if i != j {
+				for r := 0; r < p.c; r++ {
+					nonZeroElements := scalarMulFr(skShares[j], beta[i][r])
+					key0, key1, err := p.dspfN.Gen(omega[i][r], frSliceToBigIntSlice(nonZeroElements))
+					if err != nil {
+						return nil, err
+					}
+					U[i][j][r] = &DSPFKeyPair{key0, key1}
+				}
+			}
+		}
+	}
+	return U, nil
+}
+
+// embedOLECorrelations embeds OLE correlations into DSPF keys.
+func (p *PCG) embedOLECorrelations(omega, o [][][]*big.Int, beta, b [][][]*bls12381.Fr) ([][][][]*DSPFKeyPair, error) {
+	U := init4DSliceDspfKey(p.n, p.n, p.c)
+	for i := 0; i < p.n; i++ {
+		for j := 0; j < p.n; j++ {
+			if i != j {
+				for r := 0; r < p.c; r++ {
+					for s := 0; s < p.c; s++ {
+						specialPoints := outerSumBigInt(omega[i][r], o[j][s])
+						// For evaluating the performance, we allow duplicates for now
+						// if hasDuplicates(specialPoints) {
+						//	return nil, fmt.Errorf("special points contain duplicates")
+						// }
+						nonZeroElements := outerProductFr(beta[i][r], b[j][s])
+						key1, key2, err := p.dspf2N.Gen(specialPoints, frSliceToBigIntSlice(nonZeroElements))
+						if err != nil {
+							return nil, err
+						}
+						U[i][j][r][s] = &DSPFKeyPair{key1, key2}
+					}
+				}
+			}
+		}
+	}
+	return U, nil
+}
+
+// sampleExponents samples values later used as poly exponents by picking p.n*p.c random t-vectors from N.
+func (p *PCG) sampleExponents() [][][]*big.Int {
+	exp := init3DSliceBigInt(p.n, p.c, p.t)
+	for i := 0; i < p.n; i++ {
+		for j := 0; j < p.c; j++ {
+			vec := p.sampleTUniqueExponents()
+			sort.Slice(vec, func(i, j int) bool {
+				return vec[i].Cmp(vec[j]) < 0
+			})
+			exp[i][j] = vec
+		}
+	}
+	return exp
+}
+
+// sampleCoefficients samples values later used as poly coefficients by picking p.n*p.c random t-vectors from Fq.
+func (p *PCG) sampleCoefficients() [][][]*bls12381.Fr {
+	exp := init3DSliceFr(p.n, p.c, p.t)
+	for i := 0; i < p.n; i++ {
+		for j := 0; j < p.c; j++ {
+			vec := make([]*bls12381.Fr, p.t)
+			for t := range vec {
+				randElement, _ := bls12381.NewFr().Rand(p.rng)
+				vec[t] = bls12381.NewFr()
+				vec[t].Set(randElement)
+			}
+			exp[i][j] = vec
+		}
+	}
+	return exp
+}
+
+// constructPolys constructs c t-sparse polynomial from the given coefficients and exponents.
+func (p *PCG) constructPolys(coefficients [][]*bls12381.Fr, exponents [][]*big.Int) ([]*poly.Polynomial, error) {
+	if len(coefficients) != p.c {
+		return nil, fmt.Errorf("amount of coefficient slices is %d but is expected to be c=%d", len(coefficients), p.c)
+	}
+	if len(exponents) != p.c {
+		return nil, fmt.Errorf("amount of exponents slices is %d but is expected to be c=%d", len(coefficients), p.c)
+	}
+
+	res := make([]*poly.Polynomial, p.c)
+	for r := 0; r < p.c; r++ {
+		if len(coefficients[r]) != p.t {
+			return nil, fmt.Errorf("amount of coefficients is %d but is expected to be t=%d", len(coefficients[r]), p.t)
+		}
+		if len(exponents[r]) != p.t {
+			return nil, fmt.Errorf("amount of exponents is %d but is expected to be t=%d", len(coefficients[r]), p.t)
+		}
+		generatedPoly, err := poly.NewSparse(coefficients[r], exponents[r])
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate polynomial: %w", err)
+		}
+		res[r] = generatedPoly
+	}
+
+	return res, nil
+}
+
+// sampleTUniqueExponents samples t unique exponents from N.
+func (p *PCG) sampleTUniqueExponents() []*big.Int {
+	maxExp := new(big.Int).Exp(big.NewInt(2), big.NewInt(int64(p.N)), nil)
+	vec := make([]*big.Int, 0, p.t)
+	for len(vec) < p.t {
+		randNum := big.NewInt(0)
+		randNum.Rand(p.rng, maxExp)
+
+		// Check if randNum is already in vec
+		exists := false
+		for _, num := range vec {
+			if num.Cmp(randNum) == 0 {
+				exists = true
+				break
+			}
+		}
+
+		if !exists {
+			vec = append(vec, randNum)
+		}
+	}
+
+	return vec
 }
